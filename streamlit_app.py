b5d925c767f90e55c27941f64f868ba8cbef9ff4
Animesh Shukla <112156139+animeshsevak@users.noreply.github.com>
2022-08-27 21:22:14 +0530

    Update streamlit_app.py
    added code to read data from file using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
