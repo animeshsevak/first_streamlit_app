2e742a2d1883054aa1a1c54689c7992761720466
Animesh Shukla <112156139+animeshsevak@users.noreply.github.com>
2022-08-27 21:08:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text('\U0001F963 Omega 3 & Blueberry Oatmeal')
+streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-
 
